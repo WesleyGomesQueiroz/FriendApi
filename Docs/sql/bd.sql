edd16e61f182a07191c28bcdb6f43dab14a64cb6
wesle <wesle@LAPTOP-EFB6LC9O>
2021-07-13 22:41:15 -0300

    docs

---
+++ Docs/sql/bd.sql
@@ -0,0 +1,27 @@
+CREATE TABLE [tb_user] (
+  [id] int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+  [nm_nome] nvarchar (255) NOT NULL,
+  [ds_email] nvarchar(255) NOT NULL,
+  [ds_cpf] nvarchar(255),
+  [ds_senha] nvarchar(255) NOT NULL,
+  [dt_cadastro] datetime NOT NULL,
+  [dt_alteracao] datetime
+)
+GO
+
+CREATE TABLE [tb_friend] (
+  [id] int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+  [id_user] int FOREIGN KEY REFERENCES tb_user(id),
+  [nm_nome] nvarchar(255) NOT NULL,
+  [ds_email] nvarchar(255),
+  [nr_ddd] nvarchar(255),
+  [nr_telefone] nvarchar(255),
+  [ds_endereco] nvarchar(255),
+  [fl_status] bit NOT NULL,
+  [dt_cadastro] timestamp NOT NULL,
+  [dt_alteracao] datetime
+)
+GO
+
+ALTER TABLE [tb_user] ADD FOREIGN KEY ([id]) REFERENCES [tb_friend] ([id_user])
+GO
